8e67e5d25165cd5c4352938a78c28cf3e8a3c726
Tom457522 <156799445+Tom457522@users.noreply.github.com>
2026-07-10 01:16:44 +0900

    Add application lifecycle sample

---
+++ StudyWebForms/Global.asax.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Web;
+
+namespace StudyWebForms
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Application["StartedAt"] = DateTime.Now;
+            Application["TotalRequests"] = 0;
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["TotalRequests"] = ((int)(Application["TotalRequests"] ?? 0)) + 1;
+            Application.UnLock();
+        }
+    }
+}
