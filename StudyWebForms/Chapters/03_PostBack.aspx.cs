2ee9d25b9e5b88dfecb0df164266d62c2c8761b6
Tom457522 <156799445+Tom457522@users.noreply.github.com>
2026-07-10 01:18:20 +0900

    Add postback chapter code

---
+++ StudyWebForms/Chapters/03_PostBack.aspx.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace StudyWebForms.Chapters
+{
+    public partial class _03_PostBack : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ViewState["Count"] = 0;
+            }
+            RenderCount();
+        }
+
+        protected void CountButton_Click(object sender, EventArgs e)
+        {
+            ViewState["Count"] = ((int)ViewState["Count"]) + 1;
+            RenderCount();
+        }
+
+        protected void ResetButton_Click(object sender, EventArgs e)
+        {
+            ViewState["Count"] = 0;
+            RenderCount();
+        }
+
+        private void RenderCount()
+        {
+            CountLabel.Text = $"IsPostBack={IsPostBack} / Count={ViewState["Count"]}";
+        }
+    }
+}
