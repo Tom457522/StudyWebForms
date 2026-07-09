7a6e6ec58292f04c08a210da8d7f36a86444633f
Tom457522 <156799445+Tom457522@users.noreply.github.com>
2026-07-10 01:19:08 +0900

    Add sample course model

---
+++ StudyWebForms/App_Code/Course.cs
@@ -0,0 +1,10 @@
+namespace StudyWebForms
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public int DurationMinutes { get; set; }
+    }
+}
